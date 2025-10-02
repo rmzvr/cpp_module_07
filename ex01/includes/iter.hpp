2da87922536ab35d3a0b026c6de96e3159c8b404
rmzvr <rmzvr@proton.me>
2025-10-02 15:01:52 +0200

    feat(ex01): implement iter function templates for generic array manipulation

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -2,5 +2,24 @@
 # define ITER_HPP
 
 # include <iostream>
+# include <functional>
+
+template< typename T >
+void	iter(T * array, size_t length, void (*f)(T &))
+{
+	for (size_t i = 0; i < length; ++i)
+	{
+		f(array[i]);
+	}
+}
+
+template< typename T >
+void	iter(T const * array, size_t length, void (*f)(T const &))
+{
+	for (size_t i = 0; i < length; ++i)
+	{
+		f(array[i]);
+	}
+}
 
 #endif
--- ex01/sources/main.cpp
+++ ex01/sources/main.cpp
@@ -1,6 +1,37 @@
 #include <iter.hpp>
 
-int main( void ) {
+template< typename T >
+void	increment(T & i)
+{
+	i++;
+}
 
+template<>
+void	increment(std::string & i) = delete;
+
+template< typename T >
+void	print(T const & t)
+{
+	std::cout << t << std::endl;
+}
+
+int main( void ) {
+	int const			constInts[] = { 1, 2, 3, 4, 5 };
+	float				modifiableFloats[] = { 1.1f, 2.2f, 3.3f, 4.4f, 5.5f };
+
+	std::string			strings[] = { "Hi ", "world", "!" };
+	std::string const	constStrings[] = { "Hello ", "world", "!" };
+
+	iter(modifiableFloats, (sizeof(modifiableFloats) / sizeof(modifiableFloats[0])), increment<float>);
+
+	iter(modifiableFloats, (sizeof(modifiableFloats) / sizeof(modifiableFloats[0])), print<float>);
+
+	// iter(constInts, (sizeof(constInts) / sizeof(constInts[0])), increment);
+
+	iter(constInts, (sizeof(constInts) / sizeof(constInts[0])), print<int>);
+
+	// iter(strings, (sizeof(strings) / sizeof(strings[0])), increment);
+
+	iter(constStrings, (sizeof(constStrings) / sizeof(constStrings[0])), print<std::string>);
 	return 0;
 }
