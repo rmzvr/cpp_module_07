460204b886a729941b6579144547f5acb2f053d8
rmzvr <rmzvr@proton.me>
2025-10-03 12:58:50 +0200

    feat(ex02): enhance Array class with copy constructor and assignment operator

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -8,29 +8,93 @@ class Array
 {
 	private:
 		size_t	_length = 0;
-		T *		_elements;
+		T *		_elements = nullptr;
 	public:
 		Array();
 		Array( unsigned int n );
+		Array( Array<T> const & other );
+		Array<T> & operator=( Array<T> const & other );
 		~Array();
-};
 
-#endif
+		T &			operator[]( size_t idx );
+		T const &	operator[]( size_t idx ) const;
+};
 
 template <typename T>
 inline Array<T>::Array()
 {
-	this->_elements = new T[this->_length];
+	std::cout << "Default constructor called" << std::endl;
+	this->_elements = new T[0]();
 }
 
 template <typename T>
-inline Array<T>::Array(unsigned int n)
+inline Array<T>::Array( unsigned int n )
 {
-	this->_elements = new T[n];
+	std::cout << "Parameterized constructor called" << std::endl;
+	this->_length = static_cast<size_t>(n);
+	this->_elements = new T[this->_length]();
+}
+
+template <typename T>
+inline Array<T>::Array( Array<T> const &other ) : _length(other._length), _elements(new T[this->_length]())
+{
+	std::cout << "Copy constructor called" << std::endl;
+	for (size_t i = 0; i < this->_length; ++i)
+	{
+		this->_elements[i] = other._elements[i];
+	}
+}
+
+template <typename T>
+inline Array<T> &Array<T>::operator=( Array<T> const &other )
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		this->_length = other._length;
+		if (this->_elements != nullptr)
+		{
+			delete [] this->_elements;
+		}
+		this->_elements = new T[this->_length]();
+		for (size_t i = 0; i < this->_length; ++i)
+		{
+			this->_elements[i] = other._elements[i];
+		}
+	}
+	return (*this);
 }
 
 template <typename T>
 inline Array<T>::~Array()
 {
-	delete [] this->_elements;
+	std::cout << "Default destructor called" << std::endl;
+	if (this->_elements != nullptr)
+	{
+		delete [] this->_elements;
+	}
 }
+
+template <typename T>
+inline T & Array<T>::operator[]( size_t idx )
+{
+	std::cout << "Non-const subscript operator called" << std::endl;
+	if (this->_length > 0 && idx < this->_length)
+	{
+		return this->_elements[idx];
+	}
+	throw std::exception();
+}
+
+template <typename T>
+inline T const & Array<T>::operator[]( size_t idx ) const
+{
+	std::cout << "Const subscript operator called" << std::endl;
+	if (this->_length > 0 && idx < this->_length)
+	{
+		return this->_elements[idx];
+	}
+	throw std::exception();
+}
+
+#endif
--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -1,53 +1,75 @@
 #include <iostream>
 #include <Array.hpp>
 
-#define MAX_VAL 750
+#define MAX_VAL 10
 int main(int, char**)
 {
-	Array<int> numbers(MAX_VAL);
-	int* mirror = new int[MAX_VAL];
-	srand(time(NULL));
-	for (int i = 0; i < MAX_VAL; i++)
-	{
-		const int value = rand();
-		numbers[i] = value;
-		mirror[i] = value;
-	}
+	// Array<int> numbers(MAX_VAL);
+	// int* mirror = new int[MAX_VAL];
+	// srand(time(NULL));
+	// for (int i = 0; i < MAX_VAL; i++)
+	// {
+	// 	const int value = i;
+	// 	numbers[i] = value;
+	// 	mirror[i] = value;
+	// }
+
+	// {
+	// 	const Array<int> numbers(MAX_VAL);
+
+	// 	try
+	// 	{
+	// 		std::cout << numbers[9] << std::endl;
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << '\n';
+	// 	}
+	// }
+	
 	//SCOPE
 	{
+		Array<int> numbers(MAX_VAL);
 		Array<int> tmp = numbers;
-		Array<int> test(tmp);
+		// numbers[0] = 155;
+		// Array<int> test(tmp);
+		// test[0] = 0;
+		// for (size_t i = 0; i < MAX_VAL; i++)
+		// {
+		// 	std::cout << "numbers[" << i << "]: " << numbers[i] << std::endl;
+		// 	std::cout << "test[" << i << "]: " << test[i] << std::endl;
+		// }
 	}
 
-	for (int i = 0; i < MAX_VAL; i++)
-	{
-		if (mirror[i] != numbers[i])
-		{
-			std::cerr << "didn't save the same value!!" << std::endl;
-			return 1;
-		}
-	}
-	try
-	{
-		numbers[-2] = 0;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	try
-	{
-		numbers[MAX_VAL] = 0;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	// for (int i = 0; i < MAX_VAL; i++)
+	// {
+	// 	if (mirror[i] != numbers[i])
+	// 	{
+	// 		std::cerr << "didn't save the same value!!" << std::endl;
+	// 		return 1;
+	// 	}
+	// }
+	// try
+	// {
+	// 	numbers[-2] = 0;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
+	// try
+	// {
+	// 	numbers[MAX_VAL] = 0;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
 
-	for (int i = 0; i < MAX_VAL; i++)
-	{
-		numbers[i] = rand();
-	}
-	delete [] mirror;//
+	// for (int i = 0; i < MAX_VAL; i++)
+	// {
+	// 	numbers[i] = rand();
+	// }
+	// delete [] mirror;//
 	return 0;
 }
